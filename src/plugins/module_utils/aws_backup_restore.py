af08d65d72cd0d8951b484872de8bafb40ac551c
alchandia <alchandia@gmail.com>
2025-11-15 22:32:39 +0000

    feature(aws_backup_restore): add module

---
+++ src/plugins/module_utils/aws_backup_restore.py
@@ -0,0 +1,146 @@
+"""
+Util class for aws_backup_restore
+"""
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+import uuid
+import time
+import sys
+
+
+#
+# class: AWSBackupRestoreHelper
+#
+
+class AWSBackupRestoreHelper:
+    """
+    Class AWSBackupRestoreHelper
+    """
+
+    def __init__(self, module):
+        self.module = module
+
+
+    def get_efs_info(self):
+        """
+        Get information of EFS
+        """
+
+        EFS_EXIST=False
+        client_efs = boto3.client('efs', region_name=self.module.params['aws_region'])
+
+        print("Check if Filesystem exists...")
+        response = client_efs.describe_file_systems()
+        for FS in response["FileSystems"]:
+            for tags in FS["Tags"]:
+                if tags["Key"] == 'Name':
+                    if tags["Value"] == self.module.params['efs_name']:
+                        EFS_EXIST=True
+
+        return EFS_EXIST
+
+
+    def get_kms_key_id_by_alias(self, alias_name):
+        """
+        Retrieves the KMS Key ID for a given alias name.
+
+        Args:
+            alias_name (str): The alias name (e.g., "alias/my-key-alias").
+
+        Returns:
+            str: The KMS Key ID if found, None otherwise.
+        """
+        kms_client = boto3.client('kms', region_name=self.module.params['aws_region'])
+        try:
+            response = kms_client.describe_key(KeyId=alias_name)
+            key_id = response['KeyMetadata']['KeyId']
+            return key_id
+        except kms_client.exceptions.NotFoundException:
+            print(f"Alias '{alias_name}' not found.")
+            return None
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+
+
+    def efs_restore(self):
+
+        result = {
+            "changed": False,
+            "failed": False,
+            "message": []
+        }
+        CURRENT_DATE=str(datetime.today().strftime('%Y-%m-%d')) + " 00:00:00"
+        client_efs = boto3.client('efs', region_name=self.module.params['aws_region'])
+
+        print("Filesystem don't exists, restoring...")
+        client_backup = boto3.client('backup', region_name=self.module.params['aws_region'])
+
+        # Get recovery point EFS...
+        response_get_point = client_backup.list_recovery_points_by_backup_vault(
+            BackupVaultName=self.module.params['vault_name'],
+            MaxResults=1,
+            ByResourceType='EFS',
+            ByCreatedAfter=CURRENT_DATE
+        )
+
+        # Create filesystem from backup...
+        response_restore_start = client_backup.start_restore_job(
+            RecoveryPointArn=response_get_point["RecoveryPoints"][0]["RecoveryPointArn"],
+            Metadata={
+                "file-system-id": "dummy",
+                "Encrypted": "true",
+                "KmsKeyId": self.get_kms_key_id_by_alias("alias/aws/elasticfilesystem"),
+                "PerformanceMode": "generalPurpose",
+                "CreationToken": str(uuid.uuid4()),
+                "newFileSystem": "true"            
+            },
+            IamRoleArn=self.module.params['iam_role_restore'],
+            IdempotencyToken=str(uuid.uuid4()),
+            ResourceType='EFS',
+            CopySourceTagsToRestoredResource=False
+        )
+
+        # Waiting for filesystem...
+        force_exit=0
+        while True:
+            response_restore_status = client_backup.describe_restore_job(
+                RestoreJobId=response_restore_start["RestoreJobId"]
+            )
+            time.sleep(10)
+            force_exit += 1
+            status = response_restore_status["Status"] # values: 'PENDING'|'RUNNING'|'COMPLETED'|'ABORTED'|'FAILED'
+            # print("status: " + status + ", " + response_restore_status["PercentDone"])
+            if status == "COMPLETED": 
+                result['changed'] = True
+                result["message"].append("Filesystem created")
+                # Tag new filesystem...
+                response_tag = client_efs.tag_resource(
+                    ResourceId=response_restore_status["CreatedResourceArn"].split(":")[5].split("/")[1],
+                    Tags=[
+                        {
+                            'Key': 'Name',
+                            'Value': self.module.params['efs_name']
+                        },
+                    ]
+                )
+                result["message"].append("Filesystem tagged")
+                break
+
+            if status == "ABORTED" or status == "FAILED": 
+                result['failed'] = True
+                result["message"].append("Creation of filesystem was oborted or failed")
+                result["message"].append(response_restore_status)
+                break
+
+            if force_exit > 60:
+                result['failed'] = True
+                result["message"].append("The process took to long, force exit...")
+                result["message"].append("Percent done: " + response_restore_status["PercentDone"])
+
+        return result
---
+++ src/plugins/modules/aws_backup_restore.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" aws_backup_restore module """
+
+# Copyright: (c) 2018, Terry Jones <terry.jones@example.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: aws_backup_restore
+short_description: Manages restoration of AWS Backups
+version_added: "1.0.0"
+description:
+    - Manages restoration of AWS Backups
+options:
+    aws_region:
+        description:
+            - ...
+        type: str
+        required: true
+    vault_name:
+        description:
+            - ...
+        type: str
+        required: true
+    iam_role_restore:
+        description:
+            - ...
+        type: str
+        required: true
+    efs_name:
+        description:
+            - ...
+        type: str
+        required: true
+author:
+    - IT I2B (it@i2btech.com)
+'''
+
+EXAMPLES = r'''
+- name: "Restore EFS from AWS Backup"
+  i2btech.ops.aws_backup_restore:
+  aws_region: "us-east-1"
+  vault_name: "redmc-web-bak-efs-secondary"
+  iam_role_restore: "arn:aws:iam::603908928939:role/redmc-web-bak-efs-secondary"
+  efs_name: "i2btech-core-prd-webngci"
+'''
+
+RETURN = r'''
+message:
+    description: Placeholder for return value
+    type: dict
+    returned: always
+    sample: []
+'''
+
+#pylint: disable=wrong-import-position
+from ansible_collections.i2btech.ops.plugins.module_utils.aws_backup_restore import AWSBackupRestoreHelper
+from ansible.module_utils.basic import AnsibleModule
+#pylint: disable=wrong-import-position
+
+def run_module():
+    """ main module """
+
+    # define available arguments/parameters a user can pass to the module
+
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        aws_region=dict(type="str", default="us-east-1"),
+        vault_name=dict(type="str", required=True),
+        iam_role_restore=dict(type="str", required=True),
+        efs_name=dict(type="str", required=True)
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(
+        changed=False,
+        failed=False,
+        message=""
+    )
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=True
+    )
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    restored_resource = AWSBackupRestoreHelper(module)
+    result_action = {
+        "changed": False,
+        "failed": False,
+        "message": []
+    }
+
+    existing_resource = restored_resource.get_efs_info()
+
+    # Create a new resource in case it doesn't exist
+    if not existing_resource:
+        result_action = restored_resource.efs_restore()
+        result['message'] = result_action["message"]
+        result['changed'] = result_action["changed"]
+        result['failed'] = result_action["failed"]
+        
+    else:
+        result['message'] = "EFS " + module.params['efs_name'] + " exist."
+
+    if result is not None:
+        module.exit_json(**result)
+    else:
+        # in case of an unknown error
+        module.exit_json(**result)
+
+def main():
+    """ main function """
+
+    run_module()
+
+
+if __name__ == '__main__':
+    main()
