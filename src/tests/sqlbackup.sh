da1e7fd11d231fd52b7ec36d7bbc1a62d83010d4
Alvaro Chandia <alchandia@gmail.com>
2026-01-16 13:13:00 -0300

    Feature/sqlbackup postgresql (#16)
    * refactor: allow dump of postgresql databases
    
    * ci: allow push of docker image with current version tag
    
    * docs: update TODO list

--- src/tests/sqlbackup.sh
+++ src/tests/sqlbackup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-export SQL_HOST="i2btech-core-prd-i2bwebngprd.cmvnulshhnnh.us-east-1.rds.amazonaws.com"
-export SQL_USER="i2bwebngprd_user"
-export SQL_DBS="i2bwebngprd"
-export S3_BUCKET="i2btech-core-prd-logsrotate"
-export S3_PREFIX="sqlbackup"
+export SQL_TYPE="postgresql"
+export SQL_HOST="i2btechcoreprdkanban.cmvnulshhnnh.us-east-1.rds.amazonaws.com"
+export SQL_USER="manager"
+export SQL_DBS="i2btechcoreprdkanban"
+export SQL_PASS="__change_me__"
+# export S3_BUCKET="i2btech-core-prd-logsrotate"
+# export S3_PREFIX="sqlbackup"
 
 ansible-playbook i2btech.ops.sqlbackup
